b6c140549beef172da1fd61161ef981aa4f79cf0
Irvan Ariyanto <irvan.ariyanto104@gmail.com>
2018-11-14 09:04:15 +0700

    staircase solved

--- staircase.py
+++ staircase.py
@@ -8,8 +8,12 @@ import sys
 
 # Complete the staircase function below.
 def staircase(n):
-    
-
+    for i in range(0, n):
+        for j in range(n-1, i, -1):
+            print(" ",end="")
+        for j in range(0, i+1):
+            print("#",end="")
+        print()
 if __name__ == '__main__':
     n = int(input())
 
